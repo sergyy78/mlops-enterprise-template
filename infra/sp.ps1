7a3b2838f601aea6f56d4cbc57f62aca2591f6a6
sergyy78 <sergy.kozlov@gmail.com>
2026-09-19 19:04:49 +0300

    D4

--- infra/sp.ps1
+++ infra/sp.ps1
@@ -1,3 +1,6 @@
 Clear-Host
 
-az ad sp create-for-rbac --name sk-mlops-workshop-sp --role contributor --scopes /subscriptions/28008921-9aac-45be-999c-4102466a2627/resourceGroups/sk-mlops-workshop-rg --sdk-auth
+#az ad sp create-for-rbac --name sk-mlops-workshop-sp --role contributor --scopes /subscriptions/28008921-9aac-45be-999c-4102466a2627/resourceGroups/sk-mlops-workshop-rg --sdk-auth
+
+
+az ml job show --name 'mlops-enterprise-template-main_1789813540_c86627c6' --resource-group 'sk-mlops-workshop-rg'  --workspace-name 'demoAzMLWorkspace'
